01185cfab04b2725195270e62f2f5a8343b70577
Bimballer <abimbolaabdulmuizz@gmail.com>
2025-08-25 04:38:10 -0700

    fresh

--- object method/scriptmethod.js
+++ object method/scriptmethod.js
@@ -15,3 +15,22 @@ getSummary:function(){
 console.log(mahmoudobject.getSummary())
 
 console.log(mahmoudobject.calcAge())
+
+
+const mahmoudobjectt={
+    firstName:"Abdulmajeed",
+    lastName:"Mahmoud",
+    job:"Teacher",
+    age:1950,
+    friends:["Bj","Bimballer","Investor"],
+    calcAge:function(){
+    this.age=2025-this.age;
+    return this.age;
+},
+getSummary:function(){
+    return `${mahmoudobjectt.lastName} is ${mahmoudobjectt.calcAge()} years old ${mahmoudobjectt.job}`
+}
+}
+console.log(mahmoudobjectt.getSummary())
+
+console.log(mahmoudobjectt.calcAge())
